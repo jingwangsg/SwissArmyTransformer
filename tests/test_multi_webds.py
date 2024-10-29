4750b13d974507161e9081befb478c7477812c97
kningtg <kningtg@gmail.com>
2024-10-29 10:50:15 +0800

    format tests

--- tests/test_multi_webds.py
+++ tests/test_multi_webds.py
@@ -1,8 +1,25 @@
-from sat.data_utils import make_loaders
 from sat import get_args
-from sat.data_utils import MetaDistributedWebDataset
+from sat.data_utils import MetaDistributedWebDataset, make_loaders
 
-args = get_args(['--batch-size', '10', '--train-data', '/mnt/shared/img_datasets/clay1b_dataset/coyo_700m_hqaes2/part-00000/00000{0..9}.tar', '/mnt/shared/img_datasets/laion_aes_3m/part-00001/10000{0..4}.tar', '--train-data-weights', '2', '1', '--iterable-dataset', '--split','1', '--num-workers', '1', '--batch-from-same-dataset'])
+args = get_args(
+    [
+        "--batch-size",
+        "10",
+        "--train-data",
+        "/mnt/shared/img_datasets/clay1b_dataset/coyo_700m_hqaes2/part-00000/00000{0..9}.tar",
+        "/mnt/shared/img_datasets/laion_aes_3m/part-00001/10000{0..4}.tar",
+        "--train-data-weights",
+        "2",
+        "1",
+        "--iterable-dataset",
+        "--split",
+        "1",
+        "--num-workers",
+        "1",
+        "--batch-from-same-dataset",
+    ]
+)
+
 
 def process_fn(src):
     for x in src:
@@ -11,10 +28,16 @@ def process_fn(src):
         else:
             yield 0
 
-def create_func(path, args):
-    return MetaDistributedWebDataset(path, process_fn, seed=0, meta_names=['image_phash'])
 
-train_loader, val_loader, test_loader = make_loaders(args,create_dataset_function=create_func)
+def create_func(path, args):
+    return MetaDistributedWebDataset(
+        path, process_fn, seed=0, meta_names=["image_phash"]
+    )
+
+
+train_loader, val_loader, test_loader = make_loaders(
+    args, create_dataset_function=create_func
+)
 
 a, b, c = 0, 0, 0
 for i, x in enumerate(train_loader):
@@ -30,6 +53,3 @@ for i, x in enumerate(train_loader):
         c += 1
 
 print(a, b, c)
-
-
-
